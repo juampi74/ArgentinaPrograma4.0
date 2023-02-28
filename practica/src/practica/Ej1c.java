b63ed50bad40168d10e4508eb9e9980058aa37c2
Juan Pablo <juampijaca@gmail.com>
2023-02-28 20:23:25 -0300

    Subo el tercer ejercicio de la Clase 1

---
+++ practica/src/practica/Ej1c.java
@@ -0,0 +1,26 @@
+package practica;
+
+public class Ej1c {
+
+	public static void main(String[] args) {
+		
+		int numeroInicio = 5;
+		int numeroFin = 14;
+			
+		int num = numeroInicio;
+		String mostrar = "P"; 
+			
+		while(num <= numeroFin) {
+			if (mostrar == "P") {
+				if(num % 2 == 0) {
+					System.out.println(num);
+				}
+			}else {
+				if(num % 2 != 0) {
+					System.out.println(num);
+				}
+			}
+			num++;
+		}	
+	}
+}
